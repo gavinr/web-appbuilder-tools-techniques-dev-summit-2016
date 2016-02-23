59a23e05c710d64f4e0214408da516bd3c7ed86f
Gavin Rehkemper <gavreh@gmail.com>
2016-02-23 08:45:11 -0600

    updates to slides and gruntfile to make gh-pages deployments easier

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/* global module:false */
+module.exports = function(grunt) {
+	// Project configuration
+	grunt.initConfig({
+		'gh-pages': {
+			options: {
+				base: 'src/slides'
+			},
+			src: ['**/*']
+		}
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks('grunt-gh-pages');
+
+	// Default task
+	grunt.registerTask('default', []);
+	grunt.registerTask('deploy', ['default', 'gh-pages']);
+
+};
